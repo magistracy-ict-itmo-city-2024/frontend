86768329554197a985169e84e3af284a42335456
Nurgunpopov <71345993+Nurgunpopov@users.noreply.github.com>
2024-10-07 20:26:16 +0300

    add components
    add components: footer, logo and theme toggle

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { Anchor, Text } from "@mantine/core";
+import classes from "./Footer.module.css";
+
+export function Footer() {
+    return (
+        <div className={classes.footer}>
+            <div className={classes.inner}>
+                <Text size="sm">© 2023 city-24</Text>
+                <Anchor
+                    className={classes.link}
+                    href="#"
+                    underline="hover"
+                    onClick={(e) => {
+                        window.location.href = "mailto:no-reply@example.com";
+                        e.preventDefault();
+                    }}
+                    size="sm"
+                >
+                    Связаться с нами
+                </Anchor>
+            </div>
+        </div>
+    );
+}
